7ba4d3054ff9533042152fb4070e7f7d35e55b69
griffn <37548204+killgriff22@users.noreply.github.com>
2022-01-21 17:34:47 +0000

    added the installer for atloader

--- install.zsh
+++ install.zsh
@@ -1,1 +1,6 @@
-#TO BE COMMITTED
+cd $ZSH
+curl "https://raw.githubusercontent.com/killgriff22/ATLoader/main/ALhandler.py" -o ALhandler.py
+curl "https://raw.githubusercontent.com/killgriff22/ATLoader/main/ALhelp.py" -o ALhelp.py
+curl "https://raw.githubusercontent.com/killgriff22/ATLoader/main/onopen.py" -o onopen.py
+cd ~/documents/
+touch aliasesloader.zsh
