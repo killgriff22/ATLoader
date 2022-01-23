f5564490340478501f14897770d6893ab5501a5f
griffn <37548204+killgriff22@users.noreply.github.com>
2022-01-23 22:14:42 +0000

    minor bugfixes to the installer

--- install.zsh
+++ install.zsh
@@ -1,8 +1,9 @@
 cd $ZSH
-git || printf "please install git" && exit
 git clone https://github.com/killgriff22/ATLoader
 cd ATLoader
 cp @themes/ $ZSH
 cp ./*.py $ZSH -r
+cd ..
+rm -rf ATLoader
 cd ~/documents/
 touch aliasesloader.zsh
