870175d8e94137c445f695adb81b213ae04b7f0f
killgriff22 <kilgrif22033@gmail.com>
2024-07-21 11:28:05 -0400

    bring OHMYZSH up to date and make primary target windows

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,6 +1,6 @@
 # ANSI formatting function (\033[<code>m)
 # 0: reset, 1: bold, 4: underline, 22: no bold, 24: no underline, 31: red, 33: yellow
-/usr/bin/python3 ~/.oh-my-zsh/onopen.py
+python ~/.oh-my-zsh/onopen.py
 alias "@"='/usr/bin/python3 ~/.oh-my-zsh/ALhandler.py'
 omz_f() {
   [ $# -gt 0 ] || return
